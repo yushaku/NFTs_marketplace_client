1ff9309ae46ff90e8cca91c9d2f598fdf258a065
yushaku <levanson180200@gmail.com>
2024-02-20 18:21:50 +0700

    task: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,51 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    screens: {
+      xs: "400px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1220px",
+      "2xl": "1440px",
+      "3xl": "1700px",
+    },
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        abril: ["Abril Fatface", "sans-serif"],
+      },
+      colors: {
+        primary: "#2190FF",
+        black: {
+          DEFAULT: "#000",
+          100: "#0D1117",
+          200: "#161B22",
+          300: "#1F2428",
+          400: "#242C38",
+        },
+      },
+      backgroundImage: {
+        banner: "url('/jsm_resources_banner.svg')",
+      },
+      keyframes: {
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [],
+};
