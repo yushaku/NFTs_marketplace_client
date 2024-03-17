ea5edcc94071368bf3061d4f8f7a5382153e0964
Yushaku <levanson180200@gmail.com>
2024-03-17 18:42:17 +0700

    footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,3 +1,46 @@
+import { Link } from 'react-router-dom'
+import { Github } from '../icons/Github'
+import { Telegram } from '../icons/Telegram'
+import { TwitterX } from '../icons/TwitterX'
+
 export const Footer = () => {
-  return <footer>footer</footer>
+  const now = new Date().getFullYear()
+  return (
+    <footer className="my-10 flex justify-between items-center">
+      <p className="flex gap-4 text-sm">
+        <span>© {now} Yushaku DEX</span> |
+        <Link className="hover:text-accent" to={'#'}>
+          White paper
+        </Link>
+      </p>
+
+      <ul className="flex gap-5">
+        {socials.map(({ title, url, icon: Icon }) => (
+          <li key={title}>
+            <Link to={url} className="block p-3 hover:bg-layer rounded-lg">
+              <Icon className="size-5" />
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </footer>
+  )
 }
+
+const socials = [
+  {
+    icon: TwitterX,
+    title: 'Twitter',
+    url: 'https://twitter.com/daohaus'
+  },
+  {
+    icon: Github,
+    title: 'Github',
+    url: 'https://twitter.com/daohaus'
+  },
+  {
+    icon: Telegram,
+    title: 'Telegram',
+    url: 'https://twitter.com/daohaus'
+  }
+]
