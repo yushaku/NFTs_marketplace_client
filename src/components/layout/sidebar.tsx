82a00a4a1891005f95304cdc772120b350ee3cf9
Yushaku <levanson180200@gmail.com>
2024-03-30 17:49:30 +0700

    update style of navbar

--- src/components/layout/sidebar.tsx
+++ src/components/layout/sidebar.tsx
@@ -1,6 +1,6 @@
-import logo from '/logo.png'
 import { routes } from '@/utils'
 import {
+  ArrowLeftIcon,
   ArrowPathIcon,
   BanknotesIcon,
   CalendarIcon,
@@ -8,17 +8,38 @@ import {
   PaperClipIcon,
   PhotoIcon
 } from '@heroicons/react/16/solid'
+import { useState } from 'react'
 import { Link, NavLink, useLocation } from 'react-router-dom'
+import logo from '/logo.png'
 
 export const Sidebar = () => {
   const location = useLocation().pathname
+  const [isSmall, setIsSmall] = useState(false)
 
   return (
-    <section className="z-50 h-screen w-72 bg-layer">
-      <Link to="/" className="flex items-center gap-3 p-8">
+    <section
+      className={`${isSmall ? 'w-16' : 'w-72'} relative z-50 h-screen bg-layer transition-all duration-300`}
+    >
+      <Link
+        to="/"
+        className={`flex items-center gap-3 ${isSmall ? 'px-2 py-8' : 'p-8'}`}
+      >
         <img src={logo} className="size-10" alt="Vite logo" />
-        <h3 className="heading-2xl text-lighterAccent">Yushaku</h3>
+        <h3
+          className={`${isSmall ? 'hidden' : 'block'} heading-2xl text-lighterAccent`}
+        >
+          Yushaku
+        </h3>
       </Link>
+
+      <span
+        className="absolute -right-3 top-[8%] rounded-full bg-background p-2 hover:bg-focus"
+        onClick={() => setIsSmall(!isSmall)}
+      >
+        <ArrowLeftIcon
+          className={`${isSmall ? 'rotate-180' : ''} size-4 stroke-gray-600 transition-all duration-300`}
+        />
+      </span>
 
       <span className="mx-auto block h-px w-3/5 bg-gray-400/30" />
 
@@ -32,10 +53,12 @@ export const Sidebar = () => {
             <li key={href}>
               <NavLink
                 to={href}
-                className={`${pickedStyle} group flex gap-3  px-8 py-5 hover:border-l-4 hover:border-l-accent hover:bg-focus`}
+                className={`${pickedStyle} ${isSmall ? 'px-4 py-5' : 'px-8 py-5'} group flex gap-3 hover:border-l-4 hover:border-l-accent hover:bg-focus`}
               >
                 <Icon className="size-6 group-hover:fill-accent" />
-                {title}
+                <span className={`${isSmall ? 'hidden' : 'block'}`}>
+                  {title}
+                </span>
               </NavLink>
             </li>
           )
