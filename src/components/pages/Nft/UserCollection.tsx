294cbd73ec98da3604e1ced4189f33e5a82243c2
Yushaku <levanson180200@gmail.com>
2024-04-03 00:24:06 +0700

    wip

--- src/components/pages/Nft/UserCollection.tsx
+++ src/components/pages/Nft/UserCollection.tsx
@@ -1,3 +1,27 @@
+import { NFT_ADRESS } from '@/utils'
+import { useContract, useOwnedNFTs } from '@thirdweb-dev/react'
+import { useAccount } from 'wagmi'
+import NFTGrid from './NFTGrid'
+
 export const UserCollection = () => {
-  return <div>UserCollection</div>
+  const { address } = useAccount()
+  const { contract: nftCollection } = useContract(NFT_ADRESS)
+
+  const { data: ownedNfts, isLoading: loadingOwnedNfts } = useOwnedNFTs(
+    nftCollection,
+    address
+  )
+
+  return (
+    <div>
+      <h3>UserCollection</h3>
+      <p>Browse and manage your NFTs from this collection.</p>
+
+      <NFTGrid
+        data={ownedNfts}
+        isLoading={loadingOwnedNfts}
+        emptyText={"You don't own any NFTs yet from this collection."}
+      />
+    </div>
+  )
 }
