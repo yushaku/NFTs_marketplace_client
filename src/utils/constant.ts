d82e0cf2015cb6c236256387a7043325a7576186
yushaku <levanson180200@gmail.com>
2024-04-10 15:58:05 +0700

    add pinata sdk

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -9,7 +9,9 @@ export const env = cleanEnv(import.meta.env, {
   VITE_THIRD_WEB_SECRET: str(),
   VITE_OWNER_ADDRESS: str(),
   VITE_GRAPHQL_CMS_ENDPOINT: str(),
-  VITE_GRAPHQL_CMS_TOKEN: str()
+  VITE_GRAPHQL_CMS_TOKEN: str(),
+  VITE_PINATA_KEY: str(),
+  VITE_PINATA_SECRET: str()
 })
 
 export const routes = {
